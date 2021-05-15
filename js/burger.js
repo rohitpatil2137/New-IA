cf0c72ad50073e8318ed82b99a98a4e99657c483
rohitpatil2137 <69861855+rohitpatil2137@users.noreply.github.com>
2021-05-15 11:44:58 +0530

    Add files via upload

---
+++ js/burger.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("myNav").style.display = "flex";
+    document.getElementById("myNav").style.width = "100%";
+    document.getElementById("open").style.transition = "5s";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
